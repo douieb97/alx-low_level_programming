2d8658407ee8ef57b9974b55a39f5555c236ce09
Douieb Otmane <douieb791@gmail.com>
2023-06-18 01:07:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print alphapet characters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char start;
+  char end;
+  
+  start = 'a';
+  end = 'z';
+  while (start !=  end)
+  {
+    putchar(start);
+    if (start == 'z')
+      putchar('\n');
+    start++;
+  }
+  return (0);
+}
