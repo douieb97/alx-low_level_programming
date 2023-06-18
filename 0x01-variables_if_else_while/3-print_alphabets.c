245cafa6957731ebccc6f3a57b215615ba655bee
Douieb Otmane <douieb791@gmail.com>
2023-06-18 01:22:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print Alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+    char start;
+    char end;
+    char Start;
+    char End;
+    start = 'a';
+    end = 'z';
+    Start = 'A';
+    End = 'Z';
+
+    while (start <= end)
+    {
+        putchar(start);
+        start++;
+    }
+
+    while (Start <= End)
+    {
+        putchar(Start);
+        Start++;
+        if (Start > End){
+            putchar('\n');
+        }
+    }
+
+}
