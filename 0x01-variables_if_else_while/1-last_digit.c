b65aee2ae5dced1be9def72e403471ca476abe68
Douieb Otmane <douieb791@gmail.com>
2023-06-18 00:24:36 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	else if (n < 6 && n != 0)
 	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digist);
 	else
-	  printf("Last digit of %d is %d and is 0\n");
+	  printf("Last digit of %d is %d and is 0\n", n, last_digist);
 	return (0);
 }
