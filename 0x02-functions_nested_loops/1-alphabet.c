57d8946ca199aed61572b3824f79aab08565111e
Douieb Otmane <douieb791@gmail.com>
2023-06-20 03:26:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+void print_alphabet(void)
+{
+	char start, end;
+
+	start = 'a';
+	end = 'z';
+	while (start <= end)
+	{
+		_putchar(start);
+		if (start == end)
+			_putchar('\n');
+		start++;
+}	
