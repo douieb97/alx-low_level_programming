0ac7a44d341ca3081b73b572b881a780d05bb70d
Douieb Otmane <douieb791@gmail.com>
2023-06-22 15:41:30 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: number one
+ * @b: number two
+ * Return: number (Seccess)
+ */
+int _isdigit(int c)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
