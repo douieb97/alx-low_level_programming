d1c6c0c523bf5aa186aa45a760fbc90edf4d4fc8
Douieb Otmane <douieb791@gmail.com>
2023-06-22 15:20:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check character case
+ * @c character to check
+ * Return: 1 (Seccess)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
